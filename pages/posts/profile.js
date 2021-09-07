fe2e74a5fedda894b0e75ce20c7d6f2c12c02cb4
kaito <mokos.cook@gmail.com>
2021-09-07 17:17:47 +0900

    コミットテスト

--- pages/posts/profile.js
+++ pages/posts/profile.js
@@ -1,13 +1,10 @@
-import Layout from '../../components/layout'
+
 
 export default function Profill() {
   return (
     <Layout>
       <h1>First Post</h1>
       <h2>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
       </h2>
     </Layout>
   )
