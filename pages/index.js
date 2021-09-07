445d69334c47a3f3f41372c9408244e1b206c17a
kaito <mokos.cook@gmail.com>
2021-09-07 16:09:53 +0900

    コミットテスト

--- pages/index.js
+++ pages/index.js
@@ -10,11 +10,7 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-<<<<<<< HEAD
-          Wselcom to Kaito-Blogだお
-=======
-          Wselcom to Kaito-Blog!!
->>>>>>> 6fe8bed75fa0611e90b7cdc1bf6929bf5ee662d6
+Wselcom to Kaito-Blogだお
         </h1>
 
         <p className="description">
