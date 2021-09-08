0ca465d65860d74a2b94f9a5615687e88368bb42
kaito <mokos.cook@gmail.com>
2021-09-08 11:19:51 +0900

    コミットテスト

--- components/layout.js
+++ components/layout.js
@@ -1,4 +1,4 @@
-import styles from './_layout.module.scss'
+import styles from './layout.module.css'
 
 export default function Layout({ children }) {
   return <div className={styles.container}>{children}</div>
