0ea2ccd52d4939c55ef3ce3d2f47195429e0adee
kaito <mokos.cook@gmail.com>
2021-09-08 15:45:17 +0900

    コミットテスト

--- pages/posts/profile.js
+++ pages/posts/profile.js
@@ -1,9 +1,19 @@
+import Link from 'next/link'
+import Head from 'next/head'
 import Layout from '../../components/layout'
 
-export default function Profill() {
+export default function Profile() {
   return (
     <Layout>
-      <h1>First Post</h1>
+    <Head>
+    <title>Profile</title>
+    </Head>
+    <h1>Profile</h1>
+    <h2>
+    <Link href="/">
+    <a>Back to home</a>
+    </Link>
+    </h2>
     </Layout>
   )
 }
