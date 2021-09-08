5d4f2fc4674b2444deb7f7419f2fd2f60ef59bac
kaito <mokos.cook@gmail.com>
2021-09-08 18:20:09 +0900

    コミットテスト

---
+++ libs/client.js
@@ -0,0 +1,6 @@
+import { createClient } from 'microcms-js-sdk';
+
+export const client = createClient({
+  serviceDomain: 'planetatest',
+  apiKey: process.env.7c98ae2b-6364-4a73-9e42-0d8529c101de,
+});
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,40 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import { client } from "../libs/client";
 
 export default function Home() {
   return (
     <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>Hey!My name Kaito.I am react beginner.</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
+    <Head>
+    <title>{siteTitle}</title>
+    </Head>
+    <section className={utilStyles.headingMd}>
+    <p>Hey!My name Kaito.I am react beginner.</p>
+    <p>
+    (This is a sample website - you’ll be building a site like this on{' '}
+    <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+    </p>
+    </section>
+    <ul>
+    {blog.map((blog) => (
+      <li key={blog.id}>
+      <Link href={`/blog/${blog.id}`}>
+      <a>{blog.title}</a>
+      </Link>
+      </li>
+    ))}
+    </ul>
     </Layout>
   )
 }
+// データをテンプレートに受け渡す部分の処理を記述します
+export const getStaticProps = async () => {
+  const data = await client.get({ endpoint: "news" });
+
+  return {
+    props: {
+      blog: data.contents,
+    },
+  };
+};
