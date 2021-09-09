e446bbff5b49d0023bf54b163887de8cabb51041
kaito <mokos.cook@gmail.com>
2021-09-09 16:25:21 +0900

    コミットテスト

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/common.css'
-import styles from '../styles/_test.scss';
+import styles from '../styles/_test.scss'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
